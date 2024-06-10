9ebf4c8466f8af442fb0f8d2085d1164157237ce
martinekmichal <martinek.m.84@gmail.com>
2024-06-10 21:07:53 +0200

    Create L8_task5.py
    Task 5

---
+++ L8_task5.py
@@ -0,0 +1,10 @@
+def slouceni_seznamu(seznam1, seznam2):
+    spojeny = s1 + s2
+    return spojeny
+s1 = [2, 6, 8, 9, 10,]
+s2 = [3, 11, 7, 5, 13]
+spojeny = slouceni_seznamu(s1, s2)
+print(f"Seznam1 = {s1}")
+print(f"Seznam2 = {s2}")
+print(f"Sloučený seznam = {spojeny}")
+
